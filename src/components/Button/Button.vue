a1ad74d92f9423a4f66dd7cee6aa6e046b2e394a
YuQing7 <18335462829@163.com>
2026-01-14 17:04:48 +0800

    feat:button part update

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    class="vk-button"
+    :class="{
+      [`vk-button--${type}`]: type,
+      [`vk-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+  >
+    <slot></slot>
+  </button>
+</template>
+<script lang="ts" setup>
+// import type { ButtonProps } from './types'
+import type { ButtonProps } from './types'
+defineProps<ButtonProps>()
+</script>
+<style scoped></style>
