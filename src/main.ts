68bb5b5e611aafe32768aa34017731a5d85b999b
YuQing7 <18335462829@163.com>
2026-01-15 18:34:01 +0800

    button component update

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+if (import.meta.env.DEV) {
+  ;(app.config as any).devtools = true
+  app.config.performance = true // 可选：开启性能分析（调试更全面）
+}
+
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import VueMacros from 'unplugin-vue-macros'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
+    VueMacros.vite({
+      plugins: {
+        vue: vue(),
+        vueJsx: vueJsx(),
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
